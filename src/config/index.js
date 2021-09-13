34bb4b62bd6b133825671d596c0857ea6069681d
tomekmazurek <mazursky.t@gmail.com>
2021-09-13 11:27:36 +0200

    fix: edited api endpoint url

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 const environmentUrls = new Map();
-environmentUrls.set('localhost', 'http://localhost:8092');
-environmentUrls.set('mazurek-songs-vote-app.herokuapp.com', 'https://mazurek-songs-app.herokuapp.com/');
+environmentUrls.set('localhost', 'http://localhost:8092/api/v1');
+environmentUrls.set('mazurek-songs-vote-app.herokuapp.com', 'https://mazurek-songs-app.herokuapp.com/api/v1');
 
 export default environmentUrls.get(window.location.hostname);
